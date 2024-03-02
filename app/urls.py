86c0325a9c4dd63fb3a450ae07557bab48796988
Joel Rodrigues <joelsandersonrodrigues@gmail.com>
2024-03-02 17:51:58 -0300

    up

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
+from django.urls import path, include
 from rest_framework import routers
-
 from . import views
 
 router = routers.DefaultRouter()
 
 router.register("car", views.CarViewSet, basename="car")
 app_name = "app_api"
-urlpatterns = router.urls
+
+urlpatterns = [path("", views.home, name="home"), path("car/", include(router.urls))]
--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import CarSerializer
 from .models import Car
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -9,3 +10,6 @@ from .models import Car
 class CarViewSet(ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+
+def home(request):
+    return HttpResponse("deu massa")
